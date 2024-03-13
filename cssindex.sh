8776851829e506837ed12f0a3a25370a20e92e09
quisquilianart <art@quisquilian.com>
2024-03-13 15:09:12 +0000

    update text gradient

--- cssindex.sh
+++ cssindex.sh
@@ -2,6 +2,7 @@ npm run build
 fullfile=$(ls dist/assets/index*.css)
 filename="${fullfile##*/}"
 elem=$(sed -n 's/.*<link href="dist\/assets\/index-\(.*\).css" rel="stylesheet">.*/\1/p' index.html)
+echo $elem
 for html in *html
 do
 eval "sed -i 's/index-${elem}.css/${filename}/g' $html"
